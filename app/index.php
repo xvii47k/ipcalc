02cdea7b0364733e44fe3ace1262119e14a4e136
xvii47k <xavi21llinares@gmail.com>
2024-01-16 18:58:34 +0000

    Commit 1.3 de l'aplicació ipcalc

--- app/index.php
+++ app/index.php
@@ -10,5 +10,8 @@
 		<p><b>CALCUALDORA IPv4</b></p>
 		<a href="ip.html">Aplicació de càlcul de subxarxes</a>
 <p>Autor: XaviLlinares<p>
+<?php
+echo date("l jS \of F Y h:i:s A") . "<br>";
+?>
 	</body>
 <html>
